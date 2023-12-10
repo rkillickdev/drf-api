b63c4f09787d0fd9c19170059953a9b3b86660f8
Rob Killick <robkillick@me.com>
2023-12-10 13:19:31 +0000

    add search feature to PostList view

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,6 +8,8 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source=('owner.profile.id'))
     profile_image = serializers.ReadOnlyField(source=('owner.profile.image.url'))
     like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -41,5 +43,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title',
             'content', 'image', 'is_owner', 'profile_id', 'profile_image',
-            'image_filter', 'like_id'
+            'image_filter', 'like_id', 'likes_count', 'comments_count'
         ]
