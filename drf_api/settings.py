6b8ebad5e43617624c3be93f1cdd3dd93ec42f9b
Rob Killick <robkillick@me.com>
2023-12-19 15:31:55 +0000

    revert to un unified project settings in django

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
 from pathlib import Path
 import os
+import re
 import dj_database_url
 
 if os.path.exists('env.py'):
@@ -59,12 +61,27 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in os.environ
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
     'localhost',
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -15,24 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-from .views import logout_route
+from .views import root_route, logout_route
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', root_route),
     path('admin/', admin.site.urls),
-    path('api/api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     # our logout route has to be above the default one to be matched first
-    path('api/dj-rest-auth/logout/', logout_route),
-    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/logout/', logout_route),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
-        'api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
-    path('api/', include('profiles.urls')),
-    path('api/', include('posts.urls')),
-    path('api/', include('comments.urls')),
-    path('api/', include('likes.urls')),
-    path('api/', include('followers.urls')),
+    path('', include('profiles.urls')),
+    path('', include('posts.urls')),
+    path('', include('comments.urls')),
+    path('', include('likes.urls')),
+    path('', include('followers.urls')),
 ]
-
-handler404 = TemplateView.as_view(template_name='index.html')
